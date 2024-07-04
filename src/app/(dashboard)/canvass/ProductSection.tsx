5b78229dcfc15583e59f9f61181cbe2c874c279b
Gio Gonzales <gio.gonzales@carsu.edu.ph>
2024-07-04 23:49:16 +0800

    optimizes product section in canvass

--- src/app/(dashboard)/canvass/ProductSection.tsx
+++ src/app/(dashboard)/canvass/ProductSection.tsx
@@ -1,58 +1,16 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { splitUrlString } from "@/services/fetchService";
 import ProductCardSheet, { ProductType } from "./ProductCardSheet";
 import MyPagination, { LinksProp } from "@/componentUtils/MyPagination";
-
-export default function ProductSection({
+export default async function ProductSection({
   children,
+  products,
+  links,
+  urls,
 }: {
   children: React.ReactNode;
+  products?: ProductType[][] | undefined;
+  links: LinksProp[];
+  urls: string[];
 }) {
-  const [products, setProducts] = useState<Array<Array<ProductType>>>();
-  const [links, setLinks] = useState<Array<LinksProp>>([]);
-  const [hasErrors, setHasErrors] = useState<boolean>(false);
-  const [urls, setUrls] = useState<Array<string>>([]);
-  const searchParams = useSearchParams();
-  if (hasErrors) {
-    throw new Error("Something went wrong when fetching the products");
-  }
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      //get query paramater
-
-      const category = searchParams.get("category");
-
-      //for pagination
-      const currentPage = searchParams.get("page");
-
-      const result = await fetch(
-        `http://localhost:3000/api/products?category=${category}&page=${currentPage}`,
-      );
-      if (!result.ok) {
-        setHasErrors(true);
-      }
-      const fetchResult = await result.json();
-      //change this in the future
-      const links = fetchResult.products.links;
-      //memoize?
-      const prevPage = splitUrlString(fetchResult.products.prev_page_url);
-      const nextPage = splitUrlString(fetchResult.products.next_page_url);
-      setUrls([prevPage, nextPage]);
-      const filteredLinks = links.filter((link: LinksProp) => {
-        return (
-          link.label !== "&laquo; Previous" && link.label !== "Next &raquo;"
-        );
-      });
-
-      setLinks(filteredLinks);
-      setProducts(fetchResult.products.data);
-    };
-    fetchProducts();
-  }, [searchParams]);
   return products ? (
     <div className="bg-[#F8FAFC] p-5">
       <div className="grid gap-3 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 ">
--- src/app/(dashboard)/canvass/page.tsx
+++ src/app/(dashboard)/canvass/page.tsx
@@ -2,19 +2,62 @@ import RecentlyViewed from "./RecentlyViewed";
 import MainSection from "./MainSection";
 import { cookies } from "next/headers";
 import { Cart } from "@/app/actions/products";
-export default async function Canvass() {
+import { fetchWithToken, splitUrlString } from "@/services/fetchService";
+import { auth } from "@/auth";
+import { LinksProp } from "@/componentUtils/MyPagination";
+import ProductSection from "./ProductSection";
+//might transfer the fetching to the ProductSection component since it is now server
+//component also in order to juice streaming
+export default async function Canvass({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const cartCookie = cookies().get("carts");
+  const session = await auth();
+  let { category, page } = searchParams;
   if (cartCookie) {
     const cartData: Cart[] = JSON.parse(cartCookie.value);
     cartData.forEach((cart) => {
       console.log(cart.items);
     });
   }
-  //try to fetch the products here
+
+  let result = await fetchWithToken({
+    url:
+      `${process.env.BACKEND_SERVICE_URL}/api/v1/agency/products?category=` +
+      (category ? `${category}` : null) +
+      "&page=" +
+      (page ? `${page}` : null),
+    method: "GET",
+    apiToken: session?.apiToken,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  let links: LinksProp[] | [] = [];
+  const productData = await result.json();
+  const prevPage = splitUrlString(productData.products.prev_page_url);
+  const nextPage = splitUrlString(productData.products.next_page_url);
+  if (productData.products.links) {
+    links = productData.products.links.filter((link: LinksProp) => {
+      return link.label !== "&laquo; Previous" && link.label !== "Next &raquo;";
+    });
+  }
+
   return (
     <div className="grid grid-cols-12 gap-x-4 mt-5">
       <RecentlyViewed />
-      <MainSection />
+      <MainSection>
+        <ProductSection
+          urls={[prevPage, nextPage]}
+          products={productData.products.data}
+          links={links}
+        >
+          <p>Loading....</p>
+        </ProductSection>
+      </MainSection>
     </div>
   );
 }
